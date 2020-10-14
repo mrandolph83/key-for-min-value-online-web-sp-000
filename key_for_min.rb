79a398bc3e0c392e1cf2c77c28b2f41c647585cf
Matt Smith <mrandolph83@gmail.com>
2020-10-14 19:37:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0
+    return nil
+  else
+    array = []
+    name_hash.each do |name, age|
+      array << age
+    end
+    min_num = array[0]
+    array.each do |age|
+      min_num = age if age < min_num
+    end
+    name_hash.each do |name, age|
+      if min_num == vage
+        return name
+      end
+    end
+  end
 end
