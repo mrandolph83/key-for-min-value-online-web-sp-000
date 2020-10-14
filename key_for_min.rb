e2e7dec87b8050b757074b00e82e92f9a08aa894
Matt Smith <mrandolph83@gmail.com>
2020-10-14 19:41:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,16 +6,16 @@ def key_for_min_value(name_hash)
     return nil
   else
     array = []
-    name_hash.each do |name, age|
-      array << age
+    name_hash.each do |key, value|
+      array << value
     end
     min_num = array[0]
-    array.each do |age|
-      min_num = age if value < min_num
+    array.each do |value|
+      min_num = value if value < min_num
     end
-    name_hash.each do |name, value|
-      if min_num == age
-        return name
+    name_hash.each do |key, value|
+      if min_num == value
+        return key
       end
     end
   end
